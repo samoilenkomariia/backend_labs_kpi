478857077e162789842a82acd0081d74b8e75937
Samoilenko Mariia <mariia.samoilenko.06@gmail.com>
2025-11-17 23:24:34 +0200

    add password field for users table

--- lab2/expense-tracker/src/main/resources/db/migration/V1__Create_initial_tables.sql
+++ lab2/expense-tracker/src/main/resources/db/migration/V1__Create_initial_tables.sql
@@ -2,7 +2,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name VARCHAR(100) NOT NULL UNIQUE
+    name VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE category (
--- lab2/expense-tracker/src/main/resources/db/migration/V2__Insert_test_data.sql
+++ lab2/expense-tracker/src/main/resources/db/migration/V2__Insert_test_data.sql
@@ -1,7 +1,7 @@
-INSERT INTO users (id, name)
+INSERT INTO users (id, name, password)
 VALUES
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Alice'),
-    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'Bob');
+    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Alice', '$2a$10$86X3s54G/n2sA7yF5F.t7.d2t5l3d9h1P4N8m9G6J7B3C2A1O0E'),
+    ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'Bob', '$2a$10$86X3s54G/n2sA7yF5F.t7.d2t5l3d9h1P4N8m9G6J7B3C2A1O0E');
 
 INSERT INTO category (id, name, user_id)
 VALUES
@@ -23,4 +23,3 @@ VALUES
 INSERT INTO record (id, amount, record_date, user_id, category_id)
 VALUES
     ('f0eebc99-9c0b-4ef8-bb6d-6bb9bd380a33', 30.25, NOW(), 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'c0eebc99-9c0b-4ef8-bb6d-6bb9bd380a22');
-
