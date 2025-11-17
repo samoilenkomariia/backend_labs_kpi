6669646684b48764c072bc05f7424e0d7c63cdbf
Samoilenko Mariia <mariia.samoilenko.06@gmail.com>
2025-11-17 23:29:12 +0200

    add jwt entry point for auth

---
+++ lab2/expense-tracker/src/main/java/org/example/expensetracker/config/JwtAuthEntryPoint.java
@@ -0,0 +1,32 @@
+package org.example.expensetracker.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new  ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        final Map<String,Object> body = new HashMap<>();
+        body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        body.put("error", "Unauthorized");
+        body.put("message", authException.getMessage());
+        body.put("description", "Authentication token is missing or invalid");
+        body.put("path", request.getServletPath());
+        objectMapper.writeValue(response.getOutputStream(), body);
+    }
+}
